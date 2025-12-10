d9832c5f4acd8ba61702c48c4b69d6e97a8bd26b
Udeshya Sharma <114618734+udeshya17@users.noreply.github.com>
2025-12-10 20:31:11 +0530

    hardcoded data added

---
+++ src/seed/seedMenu.sql
@@ -0,0 +1,11 @@
+INSERT INTO menu_items (restaurant_id, name, price) VALUES
+(1, 'Chicken Biryani', 220),
+(1, 'Mutton Biryani', 280),
+(2, 'Chicken Biryani', 180),
+(3, 'Veg Biryani', 150),
+(4, 'Paneer Biryani', 200),
+(5, 'Chicken Biryani', 250),
+(6, 'Kolkata Biryani', 190),
+(7, 'Lucknow Biryani', 230),
+(8, 'Special Chicken Biryani', 260),
+(9, 'Andhra Biryani', 210);
